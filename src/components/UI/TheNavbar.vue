b8c894e988f49253bd7622c5cce002b0b45da33d
fluoz <hmunshi12@gmail.com>
2022-07-10 13:00:31 +0700

    add navbar and router settings

---
+++ src/components/UI/TheNavbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="h-20 bg-violet-900 w-full flex items-center justify-between">
+    <h1 class="text-white text-2xl font-bold ml-48">Find a Coach</h1>
+    <div class="mr-48">
+      <router-link class="text-lg text-purple-300 px-6 py-3 mr-2" to="/coaches"
+        >All Coaches</router-link
+      >
+      <router-link class="text-lg text-purple-300 py-3 px-6" to="/requests"
+        >Requests</router-link
+      >
+    </div>
+  </div>
+</template>
+<script setup></script>
+<style scoped>
+.router-link-exact-active {
+  border: 1px solid rgb(216 180 254);
+}
+a {
+  border: 1px solid transparent;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lato: ["Lato", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
